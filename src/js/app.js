6558a59952ee9eac214c918d541478082c757c59
vvenkov <vvenkov@htmlbox.net>
2023-01-31 21:55:05 +0200

    Done

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,18 @@
 import "../scss/app.scss";
+const R = require('ramda');
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+  const articles = document.querySelectorAll("article");
 
-  const arrayToPluck = [
+  let arrayToPluck = [
     { name: "John", class: "is-primary" },
     { age: 23, class: "is-warning" },
     { job: "programmer", class: "is-danger" },
   ];
-  const articles = document.querySelectorAll("article");
+
+  arrayToPluck = R.pluck('class', arrayToPluck)
+
+  articles.forEach((article, index) => {
+    article.classList.add(arrayToPluck[index]);
+  })
 });
